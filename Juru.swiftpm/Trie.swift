2b87bab2173c76a1689f7575343796e34985987f
AntonioPaess <paesdeandradeantonio@gmail.com>
2025-12-26 23:40:38 -0300

    Adiciona implementação da classe Trie: estrutura para gerenciamento de palavras

--- Juru.swiftpm/Trie.swift
+++ Juru.swiftpm/Trie.swift
@@ -8,5 +8,19 @@
 import Foundation
 
 class Trie {
+    private let root: TrieNode
     
+    init() {
+        root = TrieNode()
+    }
+    
+    func insert(_ word: String) {
+        guard !word.isEmpty else { return }
+        var currentNode = root
+        let characteres = Array(word.lowercased())
+        for character in characteres {
+            currentNode = currentNode.add(child: character)
+        }
+        currentNode.isTerminating = true
+    }
 }
