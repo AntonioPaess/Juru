0d1c829666fbff197854787575164ebe7ad5da06
AntonioPaess <paesdeandradeantonio@gmail.com>
2025-12-26 23:26:49 -0300

    Adiciona classes Trie e TrieNode: estrutura básica para gerenciamento de prefixos

---
+++ Juru.swiftpm/Trie.swift
@@ -0,0 +1,12 @@
+//
+//  Trie.swift
+//  Juru
+//
+//  Created by Antônio Paes De Andrade on 24/12/25.
+//
+
+import Foundation
+
+class Trie {
+    
+}
---
+++ Juru.swiftpm/TrieNode.swift
@@ -0,0 +1,35 @@
+//
+//  TrieNode.swift
+//  Juru
+//
+//  Created by Antônio Paes De Andrade on 24/12/25.
+//
+
+import Foundation
+
+class TrieNode {
+    var value: Character?
+    var children: [Character: TrieNode] = [:]
+    var isTerminating: Bool = false
+    weak var parent: TrieNode?
+    
+    init(
+        value: Character? = nil,
+        parent: TrieNode? = nil
+    ) {
+        self.value = value
+        self.parent = parent
+    }
+    
+    // Helper - Add or Create Node
+    
+    func add(child: Character) -> TrieNode {
+        if let node = children[child] {
+            return node
+        } else {
+            let node = TrieNode(value: child, parent: self)
+            children[child] = node
+            return node
+        }
+    }
+}
